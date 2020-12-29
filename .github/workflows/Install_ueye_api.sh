fb3a15297c4d50d72cb43ba4d872bd8b68734ffc
nullket <25816243+nullket@users.noreply.github.com>
2020-12-29 12:00:45 +0100

    Update Install_ueye_api.sh

--- .github/workflows/Install_ueye_api.sh
+++ .github/workflows/Install_ueye_api.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 SW_VERSION=4.94
-curl --output /tmp/ids-software-suite-linux-${SW_VERSION}-64.tgz https://de.ids-imaging.com/files/downloads/ids-software-suite/software/linux-desktop/ids-software-suite-linux-${SW_VERSION}-64.tgz
-#wget -q --tries=2 https://de.ids-imaging.com/files/downloads/ids-software-suite/software/linux-desktop/ids-software-suite-linux-${SW_VERSION}-64.tgz -P /tmp
+
+DEBIAN_FRONTEND=noninteractive apt-get -q update
+DEBIAN_FRONTEND=noninteractive apt-get -q --assume-yes install wget
+
+wget -q --tries=2 https://de.ids-imaging.com/files/downloads/ids-software-suite/software/linux-desktop/ids-software-suite-linux-${SW_VERSION}-64.tgz -P /tmp
 mkdir /tmp/ids-software-suite-linux
 tar -xzf /tmp/ids-software-suite-linux-${SW_VERSION}-64.tgz -C /tmp/ids-software-suite-linux
-sudo /tmp/ids-software-suite-linux/ueye_*_amd64.run --auto
+/tmp/ids-software-suite-linux/ueye_*_amd64.run --auto
 ldconfig -p | grep ueye
