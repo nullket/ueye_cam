83ed36cb2ab2c6e06dd7fd9f1431a052e8df073b
nullket <25816243+nullket@users.noreply.github.com>
2020-12-29 11:45:34 +0100

    Update Install_ueye_api.sh

--- .github/workflows/Install_ueye_api.sh
+++ .github/workflows/Install_ueye_api.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SW_VERSION=4.94
+
 wget -q --tries=2 https://de.ids-imaging.com/files/downloads/ids-software-suite/software/linux-desktop/ids-software-suite-linux-${SW_VERSION}-64.tgz -P /tmp
 mkdir /tmp/ids-software-suite-linux
 tar -xzf /tmp/ids-software-suite-linux-${SW_VERSION}-64.tgz -C /tmp/ids-software-suite-linux
